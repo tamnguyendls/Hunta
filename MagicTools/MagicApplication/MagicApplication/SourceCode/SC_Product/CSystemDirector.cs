f07908210ba563f2a9ac20b79ab78e9c9e0de473
pnguyen <phunghung.nguyen@datalogic.com>
2018-05-26 13:34:34 +0700

    Update for start Magic Application. *visual studio 15.

---
+++ MagicTools/MagicApplication/MagicApplication/SourceCode/SC_Product/CSystemDirector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Respond to:
+        .communicate with user environment
+        .handle system 
+*/
+
+namespace MagicApplication.SourceCode.SC_Product
+{
+    class CSystemDirector
+    {
+    }
+}
---
+++ MagicTools/MagicApplication/MagicApplication/SourceCode/SC_Product/HAL_Processors/UART_component/CUartComponent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicApplication.SourceCode.SC_Product.HAL_Processors.UART_component
+{
+    class CUartComponent
+    {
+    }
+}
