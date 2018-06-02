e3d8681df6497b056956dea6becc208d030ee01a
THANHLONG <nphung75@gmail.com>
2018-06-02 16:31:57 +0700

    Update for support ultilites function . deep copy for any object. . verify valid string.

---
+++ MagicTools/MagicApplication/MagicApplication/SourceCode/SC_Utilities/SUltilitiesFunc.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MagicApplication
+{
+    static class SUltFunc
+    {
+        public static T DeepCopy<T>(T SourceObject)
+        {
+            BinaryFormatter s = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                s.Serialize(ms, SourceObject);
+                ms.Position = 0;
+                T t = (T)s.Deserialize(ms);
+
+                return t;
+            }
+        }
+
+        public static bool VerifyString(string sData)
+        {
+            bool bRet = false;
+
+            if (sData != null && sData != string.Empty)
+            {
+                bRet = true;
+            }
+
+            return bRet;
+        }
+    }
+}
